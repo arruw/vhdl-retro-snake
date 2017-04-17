187d07b2791c583f97ee0fb7b50e820fb3db8fb7
Matjaž <matjaz.mav@gmail.com>
2017-04-17 16:07:48 +0200

    tail is follwoing but head is too fast

--- direction_queue_core.vhd
+++ direction_queue_core.vhd
@@ -24,8 +24,8 @@ end direction_queue_core;
 
 architecture Behavioral of direction_queue_core is
     signal direction : std_logic_vector (3 downto 0) := (others => '0');
-    signal direction_queue : std_logic_vector (399 downto 0) := (399 => '1', 395 => '1', others => '0'); 
-    signal direction_queue_helper : std_logic_vector (399 downto 0) := (399 => '1', 395 => '1', others => '0'); 
+    signal direction_queue : std_logic_vector (399 downto 0) := (399 => '1', 395 => '1', 391 => '1', 387 => '1', others => '0'); 
+    -- signal direction_queue_helper : std_logic_vector (399 downto 0) := (399 => '1', 395 => '1', 391 => '1', 387 => '1', others => '0'); 
 
     constant left_c : std_logic_vector (7 downto 0) := x"6b";
     constant up_c : std_logic_vector (7 downto 0) := x"75";
@@ -62,7 +62,7 @@ begin
                     when right_c    => direction <= "0010";
                     when down_c     => direction <= "0001";
                     when esc_c      => direction <= "0000";
-                    when others     => direction <= direction;           
+                    when others     => direction <= direction;
                 end case ;
             else
                 direction <= direction;
@@ -78,10 +78,10 @@ begin
                 direction_queue <= (others => '0');
             elsif push_enable_i = '1' then
                 direction_queue <= direction & direction_queue(399 downto 4);
-                direction_queue_helper <= direction & direction_queue(399 downto 4);
+                --direction_queue_helper <= direction & direction_queue(399 downto 4);
             else
-                -- direction_queue <= direction_queue;
-                direction_queue <= direction & direction_queue_helper(399 downto 4);
+                direction_queue <= direction_queue;
+                -- direction_queue <= direction & direction_queue_helper(399 downto 4);
             end if ;
         end if ;
     end process ; -- fill_direction_queue
